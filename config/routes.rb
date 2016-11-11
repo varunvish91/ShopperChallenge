9867f8f08d2caecafa60a7def1a6de9f40599738
Varun Vishwanathan <Varun.Vishwanathan9@gmail.com>
2016-11-11 09:45:19 +0000

    Added in routing to the applicant page and default UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'applicants/new'
+	root 'applicants#new'
+
   resources :applicants, only: [:create, :update, :show, :new]
   resources :funnels, only: [:index]
 end
