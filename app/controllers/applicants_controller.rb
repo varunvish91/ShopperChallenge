917424459351f6ac6b8e1a5230e0e3f42473c544
Varun Vishwanathan <Varun.Vishwanathan9@gmail.com>
2016-11-12 21:03:58 +0000

    Added in confirmation window when application is complete

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -5,11 +5,17 @@ class ApplicantsController < ApplicationController
 	end
 
   def create
-		@applicant = Applicant.new(post_params)
+		@applicant = Applicant.new
+		@applicant.first_name = params[:first_name]
+		@applicant.last_name = params[:last_name]
+		@applicant.phone = params[:phone_number]
+		@applicant.phone_type = params[:phone_type]
+		@applicant.email = params[:email]
+		@applicant.region = params[:region]
 		if @applicant.save
-			redirect_to(:back)
+			render :status => 200
 		else
-			render 'new'	
+			render :status => 400
 		end
 	end
 
@@ -21,7 +27,7 @@ class ApplicantsController < ApplicationController
 		puts "wazzzap"
 	end
 	
-	def post_params
-		params.require(:applicant).permit(:commit)
-	end
+	def destroy
+		head :ok
+	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'applicants/new'
 	root 'applicants#new'
 	
-
+	post 'applicants/create'
   resources :applicants, only: [:create, :update, :show, :new]
   resources :funnels, only: [:index]
 end
