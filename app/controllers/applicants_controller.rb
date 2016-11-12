5f04c9e7228564106bbba767230de4286d8405b8
Varun Vishwanathan <Varun.Vishwanathan9@gmail.com>
2016-11-12 21:48:10 +0000

    added in session data

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,33 +1,34 @@
 class ApplicantsController < ApplicationController
   def new
   	@applicant = Applicant.new
-		puts "new applicant"
+		session[:id] = @applicant.id
 	end
 
   def create
 		@applicant = Applicant.new
+		session[:id] = @applicant.id
 		@applicant.first_name = params[:first_name]
 		@applicant.last_name = params[:last_name]
 		@applicant.phone = params[:phone_number]
 		@applicant.phone_type = params[:phone_type]
 		@applicant.email = params[:email]
 		@applicant.region = params[:region]
-		if @applicant.save
-			render :status => 200
-		else
-			render :status => 400
-		end
+		@applicant.workflow_state = 'applied'
+		session[:first_name] = @applicant.first_name
+		session[:last_name] = @applicant.last_name
+		session[:phone] = @applicant.phone
+		session[:phone_type] = @applicant.phone_type
+		session[:email] = @applicant.email
+		session[:region] = @applicant.region
+		@applicant.save
 	end
 
   def update
-    # your code here
-  end
+		Applicant.find(session[:id]).reload
+	end
 
   def show
-		puts "wazzzap"
+		puts Applicant.count
 	end
 	
-	def destroy
-		head :ok
-	end	
 end
