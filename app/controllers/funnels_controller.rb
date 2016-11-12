ccc269806368ca04881f26fbeaceffdc06ed1c48
Varun Vishwanathan <Varun.Vishwanathan9@gmail.com>
2016-11-12 00:40:24 +0000

    Cleaned up code and improved query speed significantly

--- app/controllers/funnels_controller.rb
+++ app/controllers/funnels_controller.rb
@@ -1,25 +1,25 @@
 class FunnelsController < ApplicationController
   def index
     @funnel = {} # your code goes here
-		start_date = params[:start_date].gsub!(/\s|"|'/, '')
-		end_date   = params[:end_date].gsub!(/\s|"|'/, '')
-		
-		@entries = Applicant.where("DATE(created_at) >= ? AND DATE(created_at) <= ?", start_date, end_date)		
+		start_date = DateTime.parse(params[:start_date].gsub!(/\s|"|'/, ''))
+		end_date   = DateTime.parse(params[:end_date].gsub!(/\s|"|'/, ''))
 		@buckets = Hash.new
-		@entries.each do |c|
-			start_of_week = c.created_at.at_beginning_of_week.strftime("%Y-%m-%d")
-			end_of_week   = c.created_at.at_end_of_week.strftime("%Y-%m-%d")
-			key = start_of_week + "-" + end_of_week
-			if @buckets.has_key?(key)
-				@buckets[key]['applied'] += 1
-				if !@buckets[key].has_key?(c.workflow_state)
-					@buckets[key][c.workflow_state] = 1
-				else
-					@buckets[key][c.workflow_state] += 1
-				end	
-			else
-				create_new_key(c, key)
-			end
+		# Get the first bucket
+		first_bucket = get_bucket(start_date)
+		
+		# Get the last bucket
+		last_bucket = get_bucket(end_date)
+		
+		# Initial pull of first bucket entries
+		@buckets[first_bucket] = get_entries_for_week(start_date.at_beginning_of_week, start_date.at_end_of_week)
+		
+		while $first_bucket != $last_bucket do
+			# Increment our first_bucket
+			start_date = start_date.next_week
+				
+			# Add the entries to the next bucket
+			first_bucket = get_bucket(start_date)
+			@buckets[first_bucket] = get_entries_for_week(start_date.at_beginning_of_week, start_date.at_end_of_week)
 		end
 		hash = @buckets.to_json
 		@funnel = JSON.parse(hash)
@@ -30,12 +30,17 @@ class FunnelsController < ApplicationController
 	end
 
   private
-	def create_new_key(applicant, key)
-		@buckets[key] = Hash.new
-		@buckets[key]['applied'] = 1;
-		@buckets[key][applicant.workflow_state] = 1;
+	# Returns the work flow data for the given week
+	def get_entries_for_week(start_date, end_date)
+		return Applicant.group(:workflow_state).where("DATE(created_at) >= ? AND DATE(created_at) <= ?", start_date, end_date).count
 	end
-  # generates a formatted version of the funnel for display in d3
+
+	# Returns the bucket notation of the current date
+	def get_bucket(date)
+		return date.at_beginning_of_week.strftime("%Y-%m-%d") + "-" + date.at_end_of_week.strftime("%Y-%m-%d")
+	end
+  
+	# generates a formatted version of the funnel for display in d3
   def formatted_funnel
     formatted = Hash.new { |h, k| h[k] = [] }
 
